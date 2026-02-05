cfd056366633a1237402810cbe3928e21c1d3a1a
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2026-02-05 12:24:53 -0500

    actualizacion de la base de datos para cpanel

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,11 +1,11 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = ""; // En XAMPP normalmente está vacío
-$db   = "icv_empresa";
+$user = "icvcom_admin_icv";
+$pass = "uUuX4iDx,Cr(bELE";
+$db   = "icvcom_icv_empresa";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión");
 }

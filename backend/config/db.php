1554fa193a6c53d659114c4f29f08ade390b3dea
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2026-02-10 12:21:32 -0500

    arreglos base de datos  beta1.3

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "icvcom_admin_icv";
-$pass = "&(SCCC5zCw@QGPgA";
+$pass = "icv-vc.2117856@";
 $db   = "icvcom_icv_empresa";
 
 $conn = new mysqli($host, $user, $pass, $db);
